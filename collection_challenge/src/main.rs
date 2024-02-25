3901c691de7f395fc12911850258e06e1d0e2255
Grant Cloyd <dgcloyd@gmail.com>
2024-02-25 13:49:58 -0500

    reformat, prep for using vectors and hashmap

--- collection_challenge/src/main.rs
+++ collection_challenge/src/main.rs
@@ -2,14 +2,11 @@ use std::io::stdin;
 
 fn main() {
     println!("Welcome to the CLI employee interface!");
-
     println!("For instructions, enter help. Otherwise enter a command");
 
     loop {
         let mut input = String::new();
-
         stdin().read_line(&mut input).expect("Error");
-
         let sanitized_input = input.trim();
         
         match sanitized_input {
@@ -18,13 +15,7 @@ fn main() {
                 println!("Exiting program");
                 break; 
             }
-            _ => {
-                if handle_command(sanitized_input) {
-                    println!("This might be an acceptable phrase")
-                } else {
-                    println!("Command not recognized. Type help for more information or exit to close the program")
-                }
-            }
+            _ => { handle_command(sanitized_input) }
         };
     }
 }
@@ -39,31 +30,43 @@ fn display_help_message() {
     println!("  ** Where x is the department");
 }
 
-fn handle_command(user_input: &str) -> bool {
-        let first_idx: usize = 0; 
-        for (idx, word) in user_input.split_whitespace().enumerate() {
-            if idx == first_idx {
-                match word {
-                    "Add" => {
-                        handle_add();
-                        return true;
-                    }
-                    "View" => {
-                        handle_view();
-                        return true;
-                    },
-                    _ => { return false }
-                }
+fn handle_command(user_input: &str) {
+    let first_word = user_input.split_once(" ").unwrap_or_default().0;
+
+    match first_word {
+            "Add" => {
+                handle_add(&user_input);
             }
+            "View" => {
+                handle_view(&user_input);
+            },
+            _ => { println!("Command not recognized. Type help for more information or exit to close the program") }
         }
-        false
+}
+
+fn handle_add(input: &str) {
+    let iter = input.split_whitespace();
+
+    if check_for_length(iter.clone().count()) {
+        let user = iter.clone().nth(1).unwrap();
+        let department = iter.last().unwrap();
+        println!("Add command! {user} added to {department}")
+    } else {
+        println!("Add command not formatted properly")
     }
-
-fn handle_add() {
-    println!("Add command!")
 }
 
-fn handle_view() {
-    println!("View command!")
+fn handle_view(input: &str) {
+    let iter = input.split_whitespace();
+    
+    if check_for_length(iter.clone().count()) {
+        let department = iter.last().unwrap();
+        println!("View command! Viewing users from {department}")
+    } else {
+        println!("View command not formatted properly")
+    }
 }
 
+fn check_for_length(count: usize) -> bool {
+    count == 4 as usize
+}
